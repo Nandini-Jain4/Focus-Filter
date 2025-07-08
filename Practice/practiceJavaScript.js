669e94eefbb21c7524745bff39a9f996a2ccb786
Nandini-Jain4 <nandini0437@gmail.com>
2025-07-08 12:55:49 +0530

    Created a basic html page and practicing DOM

---
+++ Practice/practiceJavaScript.js
@@ -0,0 +1,8 @@
+// window.open;
+// window.close;
+// document.getElementById("fpara");
+// document.getElementsByClassName("textMatter");
+// document.getElementsByTagName('p');
+// document.getElementsByTagName('p');//After creation of 2nd 3rd and 4th para 
+// document.querySelector('p');
+// document.querySelectorAll('p');
