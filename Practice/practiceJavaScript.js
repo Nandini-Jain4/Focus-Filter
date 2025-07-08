197719e58f67c3eeed854e15b250772def188eb5
Nandini-Jain4 <nandini0437@gmail.com>
2025-07-08 13:52:35 +0530

    practice: Add and update DOM manipulation examples (querySelector, innerText, innerHTML)

--- Practice/practiceJavaScript.js
+++ Practice/practiceJavaScript.js
@@ -1,8 +1,31 @@
+//practice
 // window.open;
 // window.close;
+
+
+//practiceHTML1.html
 // document.getElementById("fpara");
 // document.getElementsByClassName("textMatter");
 // document.getElementsByTagName('p');
 // document.getElementsByTagName('p');//After creation of 2nd 3rd and 4th para 
 // document.querySelector('p');
 // document.querySelectorAll('p');
+
+
+//practice
+//$0
+// document.innerHTML;
+// let button = $0;
+// let button = $0;
+// button.innerHTML;
+// button.innerHTML = '<p>Hello ji Saare</p>';
+
+
+//practiceHTML2.html
+// let element = document.querySelector('#fdiv');
+// element.textContent;
+// element.innerText;
+//after adding 3rd para [style = "dispaly : none"]
+let element = document.querySelector('#fdiv');
+element.textContent;
+element.innerText;
