aa20ef242a51ccc794b79dada135db00a7b86f06
Nandini-Jain4 <nandini0437@gmail.com>
2025-07-09 12:34:02 +0530

    Update: Learned what is JSON and practice through coding the example

---
+++ Practice/jsonChaiWithCode.js
@@ -0,0 +1,23 @@
+// const course = {
+//     courseName : "js in Hindi",
+//     price: "999",
+//     courseInstructor:"hitesh"
+// }
+// course.courseInstructor
+// const {courseInstructor} = course
+// console.log(courseInstructor); 
+
+// const {courseInstructor:instructor} = course
+// console.log(instructor);
+
+//json bhi ek tarike a object hi hai but uska koi name nahi hai   
+//object nahi hai 
+//key and value are the strings (value could be a method)
+// {
+//     "name" : "hitesh",
+//     "courseName" : "js in hindi",
+//     "price": "free"
+// }
+
+// api object ya array of objects ke format mai bhii milti hai 
+
